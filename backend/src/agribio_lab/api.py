44400d4fb492f616263f87611d80d89e4b58da86
Karen Yang <212205507+coding8peach@users.noreply.github.com>
2026-09-17 14:58:26 -0700

    Allow Vercel deployment origin in CORS

--- backend/src/agribio_lab/api.py
+++ backend/src/agribio_lab/api.py
@@ -12,6 +12,7 @@ app.add_middleware(
         "http://localhost:3000",
         "http://127.0.0.1:3000",
         "https://agribio-lab.vercel.app",
+        "https://agribio-6fbk0iwtq-peach-s-project.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
