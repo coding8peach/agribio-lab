241f449648334aebab9adf20509215b39bbff48c
Karen Yang <212205507+coding8peach@users.noreply.github.com>
2026-09-17 13:46:34 -0700

    Use environment variable for API URL

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -44,7 +44,7 @@ export default function Home() {
 
     try {
       const response = await fetch(
-        "http://127.0.0.1:8000/research-projects",
+        `${process.env.NEXT_PUBLIC_API_URL}/research-projects`,
         {
           method: "POST",
           headers: {
